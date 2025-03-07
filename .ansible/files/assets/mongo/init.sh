b4b4aed80db23835aeea508446143fc6f02fc535
aissa hadjoudja <aissa.hajdoudja@akka.eu>
2025-03-07 17:54:58 +0100

    Update MongoDB init script for replica set configuration
    Revised the initialization script to use `mongosh` and added support for setting up a MongoDB replica set. Improved reliability by enhancing connection logic and waiting mechanisms. This ensures proper initialization and election of the primary node before running the database script.

--- .ansible/files/assets/mongo/init.sh
+++ .ansible/files/assets/mongo/init.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 set -e
-
-echo "Waiting for MongoDB to be available ..."
-until mongo --host localhost --quiet --eval "db.adminCommand('ping')" $> /dev/null; do
-    sleep 1;
+# Wait for MongoDB to be ready
+until mongosh --host mongo-1 --port 27017 -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin --eval "print('ok');" &>/dev/null; do
+    echo "Waiting for MongoDB to be ready..."
+    sleep 5
 done
 
-echo "Waiting for primary to be elected ..."
-until mongo --host localhost --quiet --eval "db.isMaster().ismaster" |grep true &> /dev/null; do
-  sleep 1;
+# Initialize replica set
+mongosh --host mongo-1 --port 27017 -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin --eval '
+try {
+  rs.status()
+    }
+catch(err) {
+    rs.initiate({
+      _id: "rs0",
+      members: [
+        {_id: 0, host: "mongo-1:27017", priority: 2},
+        {_id: 1, host: "mongo-2:27017", priority: 1},
+        {_id: 2, host: "mongo-3:27017", priority: 1}
+      ]
+})};'
+
+until mongosh --host mongo-1 --port 27017 -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin --eval "rs.isMaster().ismaster" | grep -q true; do
+    echo "Waiting for MongoDB replica set to initialize and primary to be elected..."
+    sleep 5
 done
 
-echo "Primary is elected. Running initialization script ..."
+echo "Executing database initiation script ... "
+mongosh --host mongo-1 --port 27017 -u "$MONGO_INITDB_ROOT_USERNAME" -p "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase admin /data/mongo-init.js
 
-mongo --host localhost /docker-entrypoint-initdb.d/mongo-init.js
-
-echo "Initialization complete !"
