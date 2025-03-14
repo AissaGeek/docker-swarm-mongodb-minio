03b45a9ed8457d78f88d37000ae5c893fa704ccb
aissa hadjoudja <aissa.hajdoudja@akka.eu>
2025-03-14 23:43:13 +0100

    Update MinIO entrypoint to use explicit data paths
    Replaced brace expansion with explicit paths for MinIO data mounts.

--- .ansible/files/assets/minio/entrypoint.sh
+++ .ansible/files/assets/minio/entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 # Start MinIO with the dynamically configured FTP address
-minio server --console-address :9001 http://minio{1...4}/minio-data \
+minio server --console-address :9001 \
+             http://minio1/data \
+             http://minio2/data \
+             http://minio3/data \
+             http://minio4/data \
     --ftp="address=${MINIO_FTP_EXTERNAL_IP}:${MINIO_FTP_PORT}" \
     --ftp="passive-port-range=${MINIO_FTP_PASV_RANGE}"
